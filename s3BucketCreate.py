bbf7f79e3bca2b7aca125cefb27df689da8c60c3
jonespanikulam <59678465+jonespanikulam@users.noreply.github.com>
2022-04-27 03:18:40 -0400

    Delete s3BucketCreate.py

--- s3BucketCreate.py
+++
@@ -1,9 +0,0 @@
-import boto3
-s3 = boto3.resource('s3')
-bucket = s3.Bucket('jones123456789')
-response = bucket.create(
-    ACL='private',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2'
-    }
-)
