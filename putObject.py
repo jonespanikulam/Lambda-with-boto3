a6e5ed36ca8e8c8bc9b810c34acf59a80d636930
jonespanikulam <59678465+jonespanikulam@users.noreply.github.com>
2022-04-27 07:31:18 -0400

    Delete putObject.py

--- putObject.py
+++
@@ -1,8 +0,0 @@
-import boto3
-s3 = boto3.client('s3')
-file_reader= open('s3BucketCreate.py').read()
-response = s3.put_object(
-    ACL='private',
-    Body= file_reader,
-    Bucket='jones123456789',
-    Key='demo')
